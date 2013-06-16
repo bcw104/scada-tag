b7420ad5abeed9beeea16e38d081c117a7e42ba9
赵磊 <zhaolei-dlut@163.com>
2013-06-16 09:56:37 +0800

    EndTag子类型添加天然气井

--- src/main/java/com/ht/scada/common/tag/util/EndTagTypeEnum.java
+++ src/main/java/com/ht/scada/common/tag/util/EndTagTypeEnum.java
@@ -7,7 +7,7 @@ package com.ht.scada.common.tag.util;
  * 
  */
 public enum EndTagTypeEnum {
-	YOU_JING("油井"), SHUI_YUAN_JING("水源井"), ZHU_SHUI_JING("注水井"), ZENG_YA_ZHAN(
+	YOU_JING("油井"), SHUI_YUAN_JING("水源井"), TIAN_RAN_QI_JING("天然气井"), ZHU_SHUI_JING("注水井"), ZENG_YA_ZHAN(
 			"增压站"), ZHU_QI_ZHAN("注汽站"), LIAN_HE_ZHAN("联合站"), JIE_ZHUAN_ZHAN(
 			"接转站"), ZHU_SHUI_ZHAN("注水站"), JI_LIANG_ZHAN("计量站"), PEI_SHUI_JIAN(
 			"配水间"), JI_LIANG_CHE("计量车");
